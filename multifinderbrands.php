fe3cea25a18827e66dac04c7540e5882aba09687
Rauka <mr.redle3@gmail.com>
2024-02-16 15:53:46 +0600

    16 February 2

--- multifinderbrands.php
+++ multifinderbrands.php
@@ -13,23 +13,24 @@ require_once 'db/db.php';
 $json = file_get_contents('php://input');
 $json = json_decode($json);
 
-var_dump($json[0]);
+$json = $json[0] ?? $json;
 
-// $sql = "SELECT * FROM images WHERE brand = :brand AND articul LIKE CONCAT('%', :articul, '%')";
-
-
-// $stmt = $pdo->prepare($sql);
-// $stmt->bindParam(':brand', $json->brand, PDO::PARAM_STR);
-// $stmt->bindParam(':articul', $json->article, PDO::PARAM_STR);
-
-// // Выполнение запроса
-// $stmt->execute();
-// $result = $stmt->fetchAll();
+$sql = "SELECT * FROM images WHERE brand = :brand AND articul LIKE CONCAT('%', :articul, '%')";
 
 
-// $data = [];
-// foreach ($result as $row) {
-//     $url = "https://233204.fornex.cloud/" . $row['brand'] . "/" . $row['articul'];
-//     array_push($data, ["url" => $url]);
-// }
-// echo json_encode($data);
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':brand', $json->brand, PDO::PARAM_STR);
+$stmt->bindParam(':articul', $json->article, PDO::PARAM_STR);
+
+// Выполнение запроса
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+
+$data = [];
+foreach ($result as $row) {
+    $url = "https://233204.fornex.cloud/" . $row['brand'] . "/" . $row['articul'];
+    array_push($data, ["url" => $url]);
+}
+header("Content-Type: application/json");
+echo json_encode($data);
