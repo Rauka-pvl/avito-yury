c92a728be930fcc6207a575cc52db32ab15c033c
Rauka <mr.redle3@gmail.com>
2024-03-04 16:18:25 +0500

    4-march3

--- create/createM.php
+++ create/createM.php
@@ -220,13 +220,14 @@ if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
                     if (response == '[]') {
                         window.location = "../view.php";
                     } else {
-                        response = JSON.parse(response);
-                        var text = "Файлы: \n";
-                        for (var i = 0; i < response.length; i++) {
-                            text = text + response[i] + "\n";
-                        }
-                        text = text + "\n Не получилось добавить!";
-                        alert(text);
+                        responseJ = JSON.parse(response);
+                        // var text = "Файлы: \n";
+                        // for (var i = 0; i < response.length; i++) {
+                        //     text = text + response[i] + "\n";
+                        // }
+                        // text = text + "\n Не получилось добавить!";
+                        alert(responseJ);
+                        alert(response);
                     }
                 },
                 error: function (error) {
