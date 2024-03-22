dbf3a274b81473a6892cef6779a94a0b749ff477
Rauka <mr.redle3@gmail.com>
2024-03-22 20:26:11 +0500

    22-march brand1

--- brand/brand.php
+++ brand/brand.php
@@ -68,33 +68,33 @@ $brands = $stmt->fetchAll(PDO::FETCH_COLUMN);
                         <button class='btn btn-primary' data-toggle='modal' data-target='#myModal$key'>Просмотр</button>
                         <button class='btn btn-warning' data-toggle='modal' data-target='#myModal2-$key'>Редактировать</button>
                         </td></tr>";
-                        $stmt1 = $pdo->query("SELECT * FROM brand_sprav WHERE brand = :brand");
-                        $stmt1->bindParam(':brand', $brand, PDO::PARAM_STR);
-                        $sprav = $stmt1->fetch(PDO::FETCH_COLUMN);
-
-                        echo "
-                        <div class='modal' id='myModal$key'>
-                            <div class='modal-dialog'>
-                                <div class='modal-content'>
-                
-                                    <!-- Заголовок модальной формы -->
-                                    <div class='modal-header'>
-                                        <h4 class='modal-title'>Просмотр</h4>
-                                    </div>
-                
-                                    <!-- Тело модальной формы -->
-                                    <div class='modal-body'>
-                                        
-                                    </div>
-                
-                                    <!-- Подвал модальной формы -->
-                                    <div class='modal-footer'>
-                                        <button type='button' class='btn btn-secondary' data-dismiss='modal'>Закрыть</button>
-                                    </div>
-                
-                                </div>
-                            </div>
-                        </div>";
+                        // $stmt1 = $pdo->query("SELECT * FROM brand_sprav WHERE brand = :brand");
+                        // $stmt1->bindParam(':brand', $brand, PDO::PARAM_STR);
+                        // $sprav = $stmt1->fetch(PDO::FETCH_COLUMN);
+                    
+                        // echo "
+                        // <div class='modal' id='myModal$key'>
+                        //     <div class='modal-dialog'>
+                        //         <div class='modal-content'>
+                    
+                        //             <!-- Заголовок модальной формы -->
+                        //             <div class='modal-header'>
+                        //                 <h4 class='modal-title'>Просмотр</h4>
+                        //             </div>
+                    
+                        //             <!-- Тело модальной формы -->
+                        //             <div class='modal-body'>
+                    
+                        //             </div>
+                    
+                        //             <!-- Подвал модальной формы -->
+                        //             <div class='modal-footer'>
+                        //                 <button type='button' class='btn btn-secondary' data-dismiss='modal'>Закрыть</button>
+                        //             </div>
+                    
+                        //         </div>
+                        //     </div>
+                        // </div>";
                     }
                     ?>
                 </tbody>
