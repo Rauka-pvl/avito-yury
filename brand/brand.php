74d8a9ea4236746c14b246463f740e957c2e2513
Rauka <mr.redle3@gmail.com>
2024-03-23 22:18:01 +0500

    23-march brand3

--- brand/brand.php
+++ brand/brand.php
@@ -179,8 +179,8 @@ $brands = $stmt->fetchAll(PDO::FETCH_COLUMN);
         let modal = $(e).parent().parent();
         let brand = modal.find('.modal-header').attr('brand');
         let sprav = modal.find('.modal-body > div > div');
-        sprav.each(function () => {
-            console.log($(tihs).find('input').val());
+        sprav.each(function () {
+            console.log($(this).find('input').val());
         });
     }
 </script>
