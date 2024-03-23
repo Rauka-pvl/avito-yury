127a4201a90787c328933ee378c6fed07bbe250f
Rauka <mr.redle3@gmail.com>
2024-03-23 21:16:18 +0500

    23-march brand3

--- brand/brand.php
+++ brand/brand.php
@@ -166,7 +166,7 @@ $brands = $stmt->fetchAll(PDO::FETCH_COLUMN);
 </body>
 <script>
     function closeI(e) {
-        console.log(e.target.parentElement);
+        console.log(e.target);
     }
 </script>
 
