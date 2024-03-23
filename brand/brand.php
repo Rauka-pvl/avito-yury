507efb968fb42423c8eb7077bc921280561c0aaf
Rauka <mr.redle3@gmail.com>
2024-03-23 22:03:30 +0500

    23-march brand3

--- brand/brand.php
+++ brand/brand.php
@@ -176,7 +176,8 @@ $brands = $stmt->fetchAll(PDO::FETCH_COLUMN);
         $(input).insertBefore($(e));
     }
     function editBrandSprav(e) {
-        console.log($(e).parent().parent());
+        console.log($(e).parent().parent().('.modal-header'));
+        console.log($(e).parent().parent().('.modal-header').brand);
     }
 </script>
 
