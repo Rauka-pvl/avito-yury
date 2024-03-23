c81471908a148b435ba8d83d242856d4eda9542f
Rauka <mr.redle3@gmail.com>
2024-03-24 00:45:16 +0500

    23-march brand3

--- db/sprav.php
+++ db/sprav.php
@@ -5,5 +5,72 @@ if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
     exit;
 }
 require_once 'db.php';
+if ($_POST['brand']) {
+    $brand = $_POST['brand'];
+} else {
+    echo json_encode('Пустой бранд!');
+    http_response_code(500);
+    exit;
+}
 
-var_dump($_POST);
+if ($_POST['sprav']) {
+    $brand = $_POST['sprav'];
+} else {
+    echo json_encode('Пустой справочник!');
+    http_response_code(500);
+    exit;
+}
+$arr = [];
+$pdo->beginTransaction();
+try {
+    $sql = "SELECT * FROM brand_sprav WHERE brand = :brand";
+    $stmt1 = $pdo->prepare($sql);
+    $stmt1->bindParam(':brand', $brand, PDO::PARAM_STR);
+    $stmt1->execute();
+    $sprav = $stmt1->fetch(PDO::FETCH_COLUMN);
+    if ($sprav) {
+        $sql1 = 'UPDATE brand_sprav SET sprav = :sprav WHERE brand = :brand';
+        $stmt = $pdo->prepare($sql);
+
+        // Привязка параметров и выполнение запроса
+        $stmt->bindParam(':brand', $brand, PDO::PARAM_STR);
+        $stmt->bindParam(':sprav', $sprav, PDO::PARAM_STR);
+
+        // Выполнение запроса
+        if ($stmt->execute()) {
+            http_response_code(200);
+        } else {
+            array_push($arr, ['error' => "Error updating data in the table: $brand/$sprav"]);
+            $pdo->commit();
+            $pdo->rollBack();
+            http_response_code(500);
+            exit;
+        }
+    } else {
+        $sql1 = 'INSERT INTO brand_sprav (brand, sprav) VALUES (:brand, :sprav)';
+        $stmt = $pdo->prepare($sql);
+
+        // Привязка параметров и выполнение запроса
+        $stmt->bindParam(':brand', $brand, PDO::PARAM_STR);
+        $stmt->bindParam(':srpav', $sprav, PDO::PARAM_STR);
+
+        // Выполнение запроса
+        if ($stmt->execute()) {
+            http_response_code(200);
+        } else {
+            array_push($arr, ['error' => "Error adding data to the table: $brand/$sprav"]);
+            $pdo->commit();
+            $pdo->rollBack();
+            http_response_code(500);
+            exit;
+        }
+    }
+
+    $pdo->commit();
+} catch (Exception $e) {
+    // Откат изменений в случае ошибки
+    $pdo->rollBack();
+    array_push($arr, ['error' => "Error processing files: " . $e->getMessage()]);
+}
+
+echo json_encode($arr);
