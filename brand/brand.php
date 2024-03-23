0baa4544c0adfe2958a273017bad55c5edb23f37
Rauka <mr.redle3@gmail.com>
2024-03-23 21:11:46 +0500

    23-march brand3

--- brand/brand.php
+++ brand/brand.php
@@ -104,7 +104,7 @@ $brands = $stmt->fetchAll(PDO::FETCH_COLUMN);
                             $edit = "<div>";
                             foreach ($s as $ss) {
                                 $sp .= $ss . "<br>";
-                                $edit .= "<div class='d-flex'><input class='form-control i-m' type='text' placholder='Бранд...' value='$ss'><span onclick='close(this)' class='close'>&times;</span></div>";
+                                $edit .= "<div class='d-flex'><input class='form-control i-m' type='text' placholder='Бранд...' value='$ss'><span onclick=" . "close(this)" . " class='close'>&times;</span></div>";
                             }
 
                             $edit .= "</div>";
