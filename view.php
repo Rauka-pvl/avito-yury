5d0b4998171c2b314ec025bb835504ef39e45f74
Рауат Аделканов <rauka@MacBook-Air-Rauat.local>
2024-04-19 15:19:27 +0500

    16-april

--- view.php
+++ view.php
@@ -22,7 +22,7 @@ if ($search && $searchA) {
     $stmt->bindParam(':search', $search, PDO::PARAM_STR);
     $stmt->bindParam(':searchA', $searchA, PDO::PARAM_STR);
 
-    $sql2 = "SELECT count() as count FROM images WHERE brand LIKE CONCAT('%', :search ,'%') AND articul LIKE CONCAT('%', :searchA ,'%') ORDER BY brand $sort LIMIT 100 OFFSET $page";
+    $sql2 = "SELECT COUNT(*) as count FROM images WHERE brand LIKE CONCAT('%', :search ,'%') AND articul LIKE CONCAT('%', :searchA ,'%') ORDER BY brand $sort LIMIT 100 OFFSET $page";
     $count = $pdo->prepare($sql2);
     $count->bindParam(':search', $search, PDO::PARAM_STR);
     $count->bindParam(':searchA', $searchA, PDO::PARAM_STR);
@@ -31,7 +31,7 @@ if ($search && $searchA) {
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':search', $search, PDO::PARAM_STR);
 
-    $sql2 = "SELECT count() as count FROM images WHERE brand LIKE CONCAT('%', :search ,'%') ORDER BY brand $sort LIMIT 100 OFFSET $page";
+    $sql2 = "SELECT COUNT(*) as count FROM images WHERE brand LIKE CONCAT('%', :search ,'%') ORDER BY brand $sort LIMIT 100 OFFSET $page";
     $count = $pdo->prepare($sql2);
     $count->bindParam(':search', $search, PDO::PARAM_STR);
 } else if ($searchA) {
@@ -39,14 +39,14 @@ if ($search && $searchA) {
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':searchA', $searchA, PDO::PARAM_STR);
 
-    $sql2 = "SELECT count() FROM images WHERE articul LIKE CONCAT('%', :searchA ,'%') ORDER BY brand $sort LIMIT 100 OFFSET $page";
+    $sql2 = "SELECT COUNT(*) FROM images WHERE articul LIKE CONCAT('%', :searchA ,'%') ORDER BY brand $sort LIMIT 100 OFFSET $page";
     $count = $pdo->prepare($sql2);
     $count->bindParam(':searchA', $searchA, PDO::PARAM_STR);
 } else {
     $sql = "SELECT * FROM images ORDER BY brand $sort LIMIT 100 OFFSET $page";
     $stmt = $pdo->prepare($sql);
 
-    $sql2 = "SELECT count() FROM images ORDER BY brand $sort LIMIT 100 OFFSET $page";
+    $sql2 = "SELECT COUNT(*) FROM images ORDER BY brand $sort LIMIT 100 OFFSET $page";
     $count = $pdo->prepare($sql2);
 }
 
