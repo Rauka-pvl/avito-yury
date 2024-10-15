452faae959e33d1e8e563f84c57a5467b27a211c
Рауат Аделканов <rauka@MacBook-Air-Rauat.local>
2024-10-15 23:01:20 +0500

    update xml.php ura

--- xml.php
+++ xml.php
@@ -13,6 +13,7 @@ if (curl_errno($ch)) {
 }
 curl_close($ch);
 $xml = simplexml_load_string($result);
+
 foreach ($xml->Ad as $ad) {
     $adId = (string) $ad->Id;
     $adId = explode('_', $adId);
@@ -21,29 +22,7 @@ foreach ($xml->Ad as $ad) {
     $stmt->bindParam(':articul', $adId[1], PDO::PARAM_STR);
     $stmt->execute();
     $row = $stmt->fetchAll();
-    // Price
-    $url = "https://www.buszap.ru/search/" . $adId[0] . "/" . $adId[1];
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $html = curl_exec($ch);
-    if (curl_errno($ch)) {
-        echo 'Error:' . curl_error($ch);
-    } else {
-        $dom = new DOMDocument;
-        @$dom->loadHTML($html);
-        $xpath = new DOMXPath($dom);
-        $prices = $xpath->query('//tr[@data-output-price]');
-        if ($prices->length > 0) {
-            $newPrice = $prices[0]->getAttribute('data-output-price'); // Новая цена
-        }
-    }
-    curl_close($ch);
-    if ($ad->Price && isset($newPrice)) {
-        unset($ad->Price);
-        $newP = $ad->addChild('Price', $newPrice);
-    }
+
     if ($row) {
         unset($ad->Images->Image);
         foreach ($row as $r) {
@@ -51,6 +30,27 @@ foreach ($xml->Ad as $ad) {
             $newImage = $ad->Images->addChild('Image', ' ');
             $newImage->addAttribute('url', $path);
             // $newImage = $ad->Images->addChild('/Image');
+        }
+    }
+
+    // Price
+    $brand0 = $adId[0];
+    $articul0 = $adId[1];
+    $url = "https://abcp50533.public.api.abcp.ru/search/articles/?userlogin=api@abcp50533&userpsw=6f42e31351bc2469f37f27a7fa7da37c&number=$articul0&brand=$brand0";
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $html = curl_exec($ch);
+    curl_close($ch);
+
+    $html = json_decode($html);
+    foreach ($html as $h) {
+        $h = (array) $h;
+        if ($h['distributorId'] == '1664240' && $h['brand'] == $brand0 && $h['number'] == $articul0) {
+            if ($ad->Price && $h['price']) {
+                unset($ad->Price);
+                $ad->addChild('Price', $h['price']);
+            }
         }
     }
 }
