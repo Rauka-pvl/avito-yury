e8f8ab1fa7d2874819a460dc4db1efff53d7738b
Рауат Аделканов <rauatadelkanov@MacBook-Air-Rauat.local>
2024-11-22 11:36:07 +0500

    xml2 0.1

--- xml2.php
+++ xml2.php
@@ -8,6 +8,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://prdownload.nodacdn.net/dfiles/7da749ad-284074-7b2184d7/articles.xml');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 $result = curl_exec($ch);
 if (curl_errno($ch)) {
     echo 'Ошибка cURL: ' . curl_error($ch);
